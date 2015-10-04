cbdae51d58ce1feefd2a1ecc8f31bddebc44f868
Md Minhazul Haque <mdminhazulhaque@gmail.com>
2015-10-04 21:26:30 +0600

    first commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# OMDb-API-Example
+
+A Python script that reads name and year info of movies, and finds out the `Genre` (or any other info) of those movies using [OMDb API](http://www.omdbapi.com/). It prints `Unknown` if no information with the movie title is found.
+
+### Input
+
+    Hotel Transylvania (2012)
+    Howl's Moving Castle (2004)
+    How To Train Your Dragon (2010)
+    Kiki's Delivery Service (1989)
+    Kung Fu Panda (2008)
+
+### Output
+
+    Hotel Transylvania | Animation, Comedy, Family
+    Howl's Moving Castle | Animation, Adventure, Family
+    How To Train Your Dragon | Animation, Adventure, Family
+    Kiki's Delivery Service | Animation, Adventure, Drama
+    Kung Fu Panda | Animation, Action, Adventure
---
+++ omdb_example.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+
+# OMDb API
+# Author: Md. Minhazul Haque
+# URL: https://minhazulhaque.com
+
+"""
+This script reads name and year info of movies, and finds out the Genre of those movies
+Prints Unknown if no movie found
+"""
+
+import json
+import requests
+import re
+
+url = 'http://www.omdbapi.com/'
+movie_list = 'movies.txt'
+
+with open(movie_list) as file:
+    for line in file.readlines():
+        line = line.strip()
+        
+        #debug
+        #if re.findall("^.*\(.*\)$", line):
+            #pass
+        #else:
+            #print line
+        #continue
+        
+        movie_name = line[:-7]
+        movie_year = line[-6:].replace("(", "").replace(")", "")
+        movie_year_i = int(movie_year)
+        
+        if movie_year_i > 1900 and movie_year_i < 2100:
+            params = dict(t=movie_name, y=movie_year)
+            resp = requests.get(url=url, params=params)
+            data = json.loads(resp.text)
+            
+            if data['Response'] == 'True':
+                print movie_name,"|",data['Genre']
+            else:
+                print movie_name,"| Unknown"
